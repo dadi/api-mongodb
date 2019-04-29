7d90e3ba2bd4b0ec252d5194cccba947940a0615
Eduardo Bouças <mail@eduardoboucas.com>
2019-04-29 19:02:39 +0100

    feat: add various fixes

--- config.js
+++ config.js
@@ -1,79 +1,95 @@
-var convict = require('convict')
-var fs = require('fs')
+const convict = require('convict')
 
-// Define a schema
-var conf = convict({
-  env: {
-    doc: "The applicaton environment.",
-    format: ["production", "development", "test", "qa"],
-    default: "development",
-    env: "NODE_ENV",
-    arg: "node_env"
-  },
-  hosts: {
-    doc: "",
-    format: Array,
-    default: [
-      {
-        host: "127.0.0.1",
-        port: 27017
-      }
-    ]
-  },
-  username: {
-    doc: "",
+const DATABASE_SCHEMA = {
+  authDatabase: {
+    doc: 'The database to authenticate against when supplying a username and password',
     format: String,
-    default: "",
-    env: "DB_USERNAME"
-  },
-  password: {
-    doc: "",
-    format: String,
-    default: "",
-    env: "DB_PASSWORD"
+    default: '',
+    env: 'DB_AUTH_SOURCE'
   },
   authMechanism: {
-    doc: "If no authentication mechanism is specified or the mechanism DEFAULT is specified, the driver will attempt to authenticate using the SCRAM-SHA-1 authentication method if it is available on the MongoDB server. If the server does not support SCRAM-SHA-1 the driver will authenticate using MONGODB-CR.",
+    doc: 'If no authentication mechanism is specified or the mechanism DEFAULT is specified, the driver will attempt to authenticate using the SCRAM-SHA-1 authentication method if it is available on the MongoDB server. If the server does not support SCRAM-SHA-1 the driver will authenticate using MONGODB-CR.',
     format: String,
-    default: "DEFAULT",
-    env: "DB_AUTH_MECHANISM"
+    default: '',
+    env: 'DB_AUTH_MECHANISM'
   },
-  authDatabase: {
-    doc: "The database to authenticate against when supplying a username and password",
+  maxPoolSize: {
+    doc: 'The maximum number of connections in the connection pool',
+    format: Number,
+    default: 0,
+    env: 'DB_MAX_POOL'
+  },
+  password: {
+    doc: '',
     format: String,
-    default: "admin",
-    env: "DB_AUTH_SOURCE"
-  },
-  database: {
-    doc: "",
-    format: String,
-    default: "test",
-    env: "DB_NAME"
-  },
-  ssl: {
-    doc: "",
-    format: Boolean,
-    default: false
-  },
-  replicaSet: {
-    doc: "",
-    format: String,
-    default: ""
+    default: '',
+    env: 'DB_PASSWORD'
   },
   readPreference: {
-    doc: "Choose how MongoDB routes read operations to the members of a replica set - see https://docs.mongodb.com/manual/reference/read-preference/",
+    doc: 'Choose how MongoDB routes read operations to the members of a replica set - see https://docs.mongodb.com/manual/reference/read-preference/',
     format: ['primary', 'primaryPreferred', 'secondary', 'secondaryPreferred', 'nearest'],
     default: 'secondaryPreferred'
   },
-  enableCollectionDatabases: {
-    doc: "",
+  replicaSet: {
+    doc: '',
+    format: String,
+    default: ''
+  },
+  ssl: {
+    doc: '',
     format: Boolean,
     default: false
+  },
+  username: {
+    doc: '',
+    format: String,
+    default: '',
+    env: 'DB_USERNAME'
   }
+}
+
+const MAIN_SCHEMA = {
+  database: {
+    default: 'api',
+    doc: 'The name of the default database to be used',
+    env: 'DB_NAME',
+    format: String
+  },
+  databases: {
+    default: {},
+    doc: 'Configuration block for each of the databases used throughout the application',
+    format: Object
+  },
+  enableCollectionDatabases: {
+    default: false,
+    doc: 'Whether to use a database specified in the collection endpoint',
+    format: Boolean
+  },
+  env: {
+    arg: 'node_env',
+    default: 'development',
+    doc: 'The applicaton environment.',
+    env: 'NODE_ENV',
+    format: ['production', 'development', 'test', 'qa']
+  }
+}
+
+const mainConfig = convict(MAIN_SCHEMA)
+
+// Load environment dependent configuration.
+const environment = mainConfig.get('env')
+
+mainConfig.loadFile(`./config/mongodb.${environment}.json`)
+mainConfig.validate()
+
+// Validating databases.
+const databases = mainConfig.get('databases')
+
+Object.keys(databases).forEach(databaseName => {
+  const databaseConfig = convict(DATABASE_SCHEMA)
+
+  databaseConfig.load(databases[databaseName])
+  databaseConfig.validate()
 })
 
-// Load environment dependent configuration
-var env = conf.get('env')
-conf.loadFile('./config/mongodb.' + env + '.json')
-
-module.exports = conf
+module.exports = mainConfig
